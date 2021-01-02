d86952c290297db91778364b46fa3ac6266775a9
Caio de Souza <caiodesouzadev@hotmail.com>
2021-01-02 10:56:38 -0300

    Criando Projeto

---
+++ src/main/java/com/caiodesouza/cursomcv/CursomcvApplication.java
@@ -0,0 +1,13 @@
+package com.caiodesouza.cursomcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcvApplication.class, args);
+	}
+
+}
